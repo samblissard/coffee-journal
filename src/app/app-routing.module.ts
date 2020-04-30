f263676397e76159d26123f4d9d1ccbac5323f7c
Sam Blissard <samblissard@gmail.com>
2020-04-29 22:58:20 -0400

    Added routing for coffee and home header links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'coffee', component: CoffeeListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
