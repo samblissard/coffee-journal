5d1f6d2a64ace65bb3e0b33553b4617788fb75a1
Sam Blissard <samblissard@gmail.com>
2020-04-29 23:15:51 -0400

    Added main page for journal entries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoffeeListComponent } from './coffee-list/coffee-list.component';
+import { JournalEntryListComponent } from './journal-entry-list/journal-entry-list.component';
 
-const routes: Routes = [{ path: 'coffee', component: CoffeeListComponent }];
+const routes: Routes = [
+  { path: 'coffee', component: CoffeeListComponent },
+  { path: 'entries', component: JournalEntryListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
