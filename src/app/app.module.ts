df7b28c9bd84e8a14b99e4bd237eeba8771d5043
Sam Blissard <samblissard@gmail.com>
2020-04-29 23:15:39 -0400

    Removing custom ripple styling in favor of button links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+
 import { HeaderComponent } from './header/header.component';
 import { CoffeeListComponent } from './coffee-list/coffee-list.component';
+import { JournalEntryListComponent } from './journal-entry-list/journal-entry-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, CoffeeListComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CoffeeListComponent,
+    JournalEntryListComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatRippleModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
